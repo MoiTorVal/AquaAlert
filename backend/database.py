0b56636d45a1d1816e2d2d3ec85cdcd66d6e243a
moises valdez <moisesvaldez0559@gmail.com>
2026-03-20 19:07:12 -0700

    feat:  add database connection with SQLAlchemy

---
+++ backend/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+if __name__ == "__main__":
+    try:
+        with engine.connect() as connection:
+            print("Database connection successful")
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
